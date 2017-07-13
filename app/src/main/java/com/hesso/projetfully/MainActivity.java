671a5ad70692dc124bc3b1a4941184b9920469dc
Axel <ax.be666@hotmail.com>
2017-07-13 18:14:51 +0200

    testa

--- app/src/main/java/com/hesso/projetfully/MainActivity.java
+++ app/src/main/java/com/hesso/projetfully/MainActivity.java
@@ -3,6 +3,7 @@ package com.hesso.projetfully;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//blablabla
 public class MainActivity extends AppCompatActivity {
 
     @Override
