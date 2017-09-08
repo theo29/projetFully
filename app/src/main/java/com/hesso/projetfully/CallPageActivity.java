5253d389d462ea410e20437488a87a5ec4320640
Axel <axel.berclaz@students.hevs.ch>
2017-09-08 11:38:05 +0200

    Add empty map

--- app/src/main/java/com/hesso/projetfully/CallPageActivity.java
+++ app/src/main/java/com/hesso/projetfully/CallPageActivity.java
@@ -125,4 +125,10 @@ public class CallPageActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void onClickShowiInMap(View view){
+        Intent intent = new Intent(this, CallMapsActivity.class);
+        intent.putExtra(MODIFY_CALL, call.getLieu());
+        startActivity(intent);
+    }
+
 }
